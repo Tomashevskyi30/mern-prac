f64b210da5e602004863c103d4aae0cc24ae9459
Tomashevskyi30 <tomashevskyi30@gmail.com>
2022-06-01 15:58:15 +0300

    start rework project

--- client/src/pages/CreatePage.js
+++ client/src/pages/CreatePage.js
@@ -14,14 +14,12 @@ export const CreatePage = () => {
     }, [])
 
     const pressHandler = async event => {
-        if (event.key === 'Enter') {
             try {
                 const data = await request('/api/link/generate', 'POST', {from: link}, {
                     Authorization: `Bearer ${auth.token}`
                 })
                 history.push(`/detail/${data.link._id}`)
             } catch (e) {}
-        }
     }
 
     return (
@@ -29,14 +27,15 @@ export const CreatePage = () => {
             <div className="col s8 offset-s2" style={{paddingTop: '2rem'}}>
                 <div className="input-field">
                     <input
-                        placeholder="Вставьте ссылку"
+                        placeholder="Link"
                         id="link"
                         type="text"
                         value={link}
                         onChange={e => setLink(e.target.value)}
                         onKeyPress={pressHandler}
                     />
-                    <label htmlFor="link">Введите ссылку</label>
+                    <label htmlFor="link">Input your link</label>
+                    <button onClick={pressHandler} className="waves-effect waves-light btn">Create</button>
                 </div>
             </div>
         </div>
